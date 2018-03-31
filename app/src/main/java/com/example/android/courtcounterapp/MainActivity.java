fa5d0f9d7b43d866321e68496b8e4a22e4172bb6
Yana Denisenko <yanadmail@gmail.com>
2018-03-31 15:12:42 -0700

    fixed the bug causing score to zero after screen rotation; created global instance variables for TextViews and initialized them in onCreate() method

--- app/src/main/java/com/example/android/courtcounterapp/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounterapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.courtcounterapp;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,23 +14,35 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scorePlayerA = 0;
-    int scorePlayerB = 0;
-    int triesPlayerA = 0;
-    int triesPlayerB = 0;
+    /**
+     * These variable are destroyed along with Activity
+     */
+    private int scorePlayerA;
+    private int scorePlayerB;
+    private int triesPlayerA;
+    private int triesPlayerB;
+    private TextView scoreViewPlayerA;
+    private TextView scoreViewPlayerB;
+    private TextView triesViewPlayerA;
+    private TextView triesViewPlayerB;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreViewPlayerA = (TextView) findViewById(R.id.player_a_score);
+        triesViewPlayerA = (TextView) findViewById(R.id.player_a_tries);
+        scoreViewPlayerB = (TextView) findViewById(R.id.player_b_score);
+        triesViewPlayerB = (TextView) findViewById(R.id.player_b_tries);
     }
 
     /**
      * Displays the given score for Player A.
      */
     public void displayForPlayerA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.player_a_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewPlayerA.setText(String.valueOf(score));
     }
 
     /**
@@ -45,8 +59,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the tries for Player A.
      */
     public void displayTriesForPlayerA(int score) {
-        TextView triesView = (TextView) findViewById(R.id.player_a_tries);
-        triesView.setText(String.valueOf(score));
+        triesViewPlayerA.setText(String.valueOf(score));
     }
 
     /**
@@ -61,8 +74,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Player B.
      */
     public void displayForPlayerB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.player_b_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewPlayerB.setText(String.valueOf(score));
     }
 
     /**
@@ -79,8 +91,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the tries for Player B.
      */
     public void displayTriesForPlayerB(int score) {
-        TextView triesView = (TextView) findViewById(R.id.player_b_tries);
-        triesView.setText(String.valueOf(score));
+        triesViewPlayerB.setText(String.valueOf(score));
     }
 
     /**
@@ -92,13 +103,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method reset all scores for all teams.
+     * This method reset all scores for all teams
      */
     public void resetAllScores(View v) {
         scorePlayerB = 0;
         scorePlayerA = 0;
         triesPlayerA = 0;
         triesPlayerB = 0;
+        displayForPlayerA(scorePlayerA);
+        displayForPlayerB(scorePlayerB);
+        displayTriesForPlayerA(triesPlayerA);
+        displayTriesForPlayerB(triesPlayerB);
+    }
+
+    /**
+     * This method saves current Views state before screen rotation
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("scorePlayerA", scorePlayerA);
+        savedInstanceState.putInt("scorePlayerB", scorePlayerB);
+        savedInstanceState.putInt("triesPlayerA", triesPlayerA);
+        savedInstanceState.putInt("triesPlayerB", triesPlayerB);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    /**
+     * This method restores current views state after screen rotation
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        scorePlayerA = savedInstanceState.getInt("scorePlayerA");
+        scorePlayerB = savedInstanceState.getInt("scorePlayerB");
+        triesPlayerA = savedInstanceState.getInt("triesPlayerA");
+        triesPlayerB = savedInstanceState.getInt("triesPlayerB");
         displayForPlayerA(scorePlayerA);
         displayForPlayerB(scorePlayerB);
         displayTriesForPlayerA(triesPlayerA);
